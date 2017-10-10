8162646e233c712aec8f18e72cd4049bf798914a
stankant <stankovsky.ant@gmail.com>
2017-10-10 16:39:52 +0300

    fix: unactivated error

--- src/services/passportMiddleware.ts
+++ src/services/passportMiddleware.ts
@@ -15,9 +15,6 @@ export const jwtLogin = async (ctx: Koa.Context, next: any) => {
           throw new AuthError(codeErrors.AUTH_ERROR);
         }
         if (user) {
-          if (!user.confirmed) {
-            throw new AuthError(codeErrors.ACCOUNT_NOT_ACTIVATED);
-          }
           await next();
         } else {
           throw new AuthError(codeErrors.AUTH_ERROR);
@@ -40,6 +37,9 @@ export const localLogin = async (ctx: Koa.Context, next: any) => {
         throw new AuthError(codeErrors.INCORRECT_EMAIL_OR_PASS);
       }
       if (user) {
+        if (!user.confirmed) {
+          throw new AuthError(codeErrors.ACCOUNT_NOT_ACTIVATED);
+        }
         ctx.state.user = user;
         await next();
       }
