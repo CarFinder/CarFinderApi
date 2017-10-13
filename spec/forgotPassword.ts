bd27dd3e169440d8087e477e6775b4444448d741
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-13 15:48:12 +0300

    Refractor tests for restore password with async await

--- spec/forgotPassword.ts
+++ spec/forgotPassword.ts
@@ -9,55 +9,58 @@ import app from './index';
 chai.use(chaiHttp);
 
 describe('Forgot password', () => {
-  before(done => {
+  before(async () => {
     const validUser = {
       confirmed: true,
       email: 'validemail@test.com',
       name: 'ValidName',
       password: 'Password1#'
     };
-    User.create(validUser).then(() => done());
+    await User.create(validUser);
   });
 
-  it('should throw a security error, if there is no email in the database', done => {
-    chai
-      .request(app)
-      .post('/api/user/forgot')
-      .set('content-type', 'application/json')
-      .send({ email: 'nosuchemail@test.com' })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.UNAUTHORIZED);
-        assert.equal(codeErrors.INCORRECT_EMAIL_OR_PASS, res.body.error.code);
-        done();
-      });
+  it('should throw a security error, if there is no email in the database', async () => {
+    try {
+      await chai
+        .request(app)
+        .post('/api/user/forgot')
+        .set('content-type', 'application/json')
+        .send({ email: 'nosuchemail@test.com' });
+      assert.fail('Test failed');
+    } catch (error) {
+      error.response.should.have.status(HttpStatus.UNAUTHORIZED);
+      assert.equal(codeErrors.INCORRECT_EMAIL_OR_PASS, error.response.body.error.code);
+    }
   });
 
-  it("should throw a validation error, if an email doesn't pass validation rules", done => {
-    chai
-      .request(app)
-      .post('/api/user/forgot')
-      .set('content-type', 'application/json')
-      .send({ email: 'invalidemail' })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.UNAUTHORIZED);
-        assert.equal(codeErrors.VALIDATION_ERROR, res.body.error.code);
-        done();
-      });
+  it("should throw a validation error, if an email doesn't pass validation rules", async () => {
+    try {
+      await chai
+        .request(app)
+        .post('/api/user/forgot')
+        .set('content-type', 'application/json')
+        .send({ email: 'invalid' });
+      assert.fail('Test failed');
+    } catch (error) {
+      error.response.should.have.status(HttpStatus.UNAUTHORIZED);
+      assert.equal(codeErrors.VALIDATION_ERROR, error.response.body.error.code);
+    }
   });
 
-  it('should be status OK, if an email passes validation rules and exists in the DB', done => {
-    chai
-      .request(app)
-      .post('/api/user/forgot')
-      .set('content-type', 'application/json')
-      .send({ email: 'validemail@test.com' })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.OK);
-        done();
-      });
+  it('should be status OK, if an email passes validation rules and exists in the DB', async () => {
+    try {
+      const response = await chai
+        .request(app)
+        .post('/api/user/forgot')
+        .set('content-type', 'application/json')
+        .send({ email: 'validemail@test.com' });
+      response.should.have.status(HttpStatus.OK);
+    } catch {
+      assert.fail('Test failed');
+    }
   });
 
-  after(done => {
-    User.remove({ email: 'validemail@test.com' }).then(() => done());
+  after(async () => {
+    await User.remove({ email: 'validemail@test.com' });
   });
 });
--- spec/restorePassword.ts
+++ spec/restorePassword.ts
@@ -18,95 +18,100 @@ describe('Restore Password', () => {
   };
   const validToken = getToken({ email: validUser.email });
 
-  before(done => {
-    User.create(validUser).then(() => done());
+  before(async () => {
+    await User.create(validUser);
   });
 
-  it("should throw a validation error, if a password doesn't pass validation rules", done => {
-    chai
-      .request(app)
-      .post('/api/user/restore')
-      .set('content-type', 'application/json')
-      .send({
-        data: {
-          password: 'invalidpassword',
-          token: validToken
-        }
-      })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.UNAUTHORIZED);
-        assert.equal(codeErrors.VALIDATION_ERROR, res.body.error.code);
-        done();
-      });
+  it("should throw a validation error, if a password doesn't pass validation rules", async () => {
+    try {
+      await chai
+        .request(app)
+        .post('/api/user/restore')
+        .set('content-type', 'application/json')
+        .send({
+          data: {
+            password: 'invalidpassword',
+            token: validToken
+          }
+        });
+      assert.fail('Test failed');
+    } catch (error) {
+      error.response.should.have.status(HttpStatus.UNAUTHORIZED);
+      assert.equal(codeErrors.VALIDATION_ERROR, error.response.body.error.code);
+    }
   });
 
-  it('should throw an auth error, if no token is provided', done => {
-    chai
-      .request(app)
-      .post('/api/user/restore')
-      .set('content-type', 'application/json')
-      .send({ data: { password: 'Password2#' } })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.UNAUTHORIZED);
-        assert.equal(codeErrors.AUTH_ERROR, res.body.error.code);
-        done();
-      });
+  it('should throw an auth error, if no token is provided', async () => {
+    try {
+      await chai
+        .request(app)
+        .post('/api/user/restore')
+        .set('content-type', 'application/json')
+        .send({ data: { password: 'Password2#' } });
+      assert.fail('Test failed');
+    } catch (error) {
+      error.response.should.have.status(HttpStatus.UNAUTHORIZED);
+      assert.equal(codeErrors.AUTH_ERROR, error.response.body.error.code);
+    }
   });
 
-  it('should throw a JWT Decode error, if token is not valid', done => {
-    chai
-      .request(app)
-      .post('/api/user/restore')
-      .set('content-type', 'application/json')
-      .send({
-        data: {
-          password: 'Password2#',
-          token: 'ey123'
-        }
-      })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.UNAUTHORIZED);
-        assert.equal(codeErrors.JWT_DECODE_ERROR, res.body.error.code);
-        done();
-      });
+  it('should throw a JWT Decode error, if token is not valid', async () => {
+    try {
+      await chai
+        .request(app)
+        .post('/api/user/restore')
+        .set('content-type', 'application/json')
+        .send({
+          data: {
+            password: 'Password2#',
+            token: 'ey123'
+          }
+        });
+      assert.fail('Test failed');
+    } catch (error) {
+      error.response.should.have.status(HttpStatus.UNAUTHORIZED);
+      assert.equal(codeErrors.JWT_DECODE_ERROR, error.response.body.error.code);
+    }
   });
 
-  it("should throw an error, if can'not update password with such email", done => {
-    chai
-      .request(app)
-      .post('/api/user/restore')
-      .set('content-type', 'application/json')
-      .send({
-        data: {
-          password: 'Password2#',
-          token: getToken({ email: 'invalidemail@test.com' })
-        }
-      })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.UNAUTHORIZED);
-        assert.equal(codeErrors.INCORRECT_EMAIL_OR_PASS, res.body.error.code);
-        done();
-      });
+  it("should throw an error, if can'not update password with such email", async () => {
+    try {
+      await chai
+        .request(app)
+        .post('/api/user/restore')
+        .set('content-type', 'application/json')
+        .send({
+          data: {
+            password: 'Password2#',
+            token: getToken({ email: 'invalidemail@test.com' })
+          }
+        });
+      assert.fail('Test failed');
+    } catch (error) {
+      error.response.should.have.status(HttpStatus.UNAUTHORIZED);
+      assert.equal(codeErrors.INCORRECT_EMAIL_OR_PASS, error.response.body.error.code);
+    }
   });
 
-  it('should be status OK, if token and new password are valid', done => {
-    chai
-      .request(app)
-      .post('/api/user/restore')
-      .set('content-type', 'application/json')
-      .send({
-        data: {
-          password: 'Password2#',
-          token: validToken
-        }
-      })
-      .end((err, res) => {
-        res.should.have.status(HttpStatus.OK);
-        done();
-      });
+  it('should be status OK, if token and new password are valid', async () => {
+    try {
+      const response = await chai
+        .request(app)
+        .post('/api/user/restore')
+        .set('content-type', 'application/json')
+        .send({
+          data: {
+            password: 'Password2#',
+            token: validToken
+          }
+        });
+      response.should.have.status(HttpStatus.OK);
+    } catch (error) {
+      assert.fail('Test failed');
+    }
   });
 
-  after(done => {
-    User.remove({ email: validUser.email }).then(() => done());
+  after(async () => {
+    await User.remove({ email: validUser.email });
   });
 });
