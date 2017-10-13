b3ff0c9280da0287451b962e21608497e8e1fd86
Elijah <ilya.bobr.thj@gmail.com>
2017-10-13 14:54:22 +0300

    fix:mark repository

--- src/repositories/markRepository.ts
+++ src/repositories/markRepository.ts
@@ -1,7 +1,10 @@
 import * as mongoose from 'mongoose';
-import
+import { IMarkModel, Mark } from '../db';
 import { IMark } from '../interfaces';
 
-export const create = async (model:IMark) => {
-  const mark = new 
-}
+export const create = async (mark: IMark) => {
+  const newMark = new Mark(mark);
+  await newMark.save(err => {
+    return err;
+  });
+};
